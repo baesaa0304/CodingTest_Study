a24c966dc015ad827c034291397818efe7be66c1
baesaa0304 <baesaa0304@naver.com>
2024-12-30 16:38:56 +0900

    수

--- Baesunyoung/src/Baekjoon/Sliver/baekjoon_13414/baekjoon_13414_wrong.java
+++ Baesunyoung/src/Baekjoon/Sliver/baekjoon_13414/baekjoon_13414_wrong.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class baekjoon_13414_wrong {
 
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int K = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
