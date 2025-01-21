8179179d3b441a23212790addb3d30352065a8c4
Kim06 <Kim06@jimin_noteBook>
2025-01-21 22:51:22 +0900

    0121(화)_2×n 타일링_성공

--- Kimjimin/src/Baekjoon/Silver/No11726_2xnTiling.java
+++ Kimjimin/src/Baekjoon/Silver/No11726_2xnTiling.java
@@ -9,20 +9,21 @@ public class No11726_2xnTiling {
 		int n = sc.nextInt();
 		
 		// DP 배열 선언
-		int[] dp = new int[n+1];
+		int[] dp = new int[1001];
 		
 		// dp 초기값 설정
 		dp[1] = 1;
 		dp[2] = 2;
 		
 		// dp 구하는 코드
+		// i = 46~, int형 범위(10자리)를 넘음
+		// 최종 % 10007을 나누는 것이 아닌 중간 과정에서 나누어야 함.
 		for(int i = 3; i <= n; i++) {
-			dp[i] = dp[i-1] + dp[i-2];
-			System.out.println("확인:" + dp[i]);
+			dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+			System.out.println("확인:" + dp[i] + "i의 값:" + i);
 		}
 		System.out.println("최종 확인:" + dp[n]);
-		int result = dp[n] % 10007;
-		System.out.println(result);
+		System.out.println(dp[n]);
 				
 
 	}
