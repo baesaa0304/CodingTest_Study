a03dd177e77cebe19a3994d56205288a356d7b97
jimin Kim <gjimin26@gmail.com>
2025-04-16 23:22:49 +0900

    김지민_pull (오류 수정 후 pull)

---
+++ Baesunyoung/src/Baekjoon/Sliver/baejoon_6603/baekjoon_6603.java
@@ -0,0 +1,41 @@
+package Baekjoon.Sliver.baejoon_6603;
+
+import java.io.*;
+import java.util.*;
+
+
+public class baekjoon_6603 {
+	static StringBuilder sb = new StringBuilder();
+    static int[] lotto;
+    static int K;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            K = Integer.parseInt(st.nextToken());
+            if (K == 0) break;
+            lotto = new int[K];
+            for(int i = 0; i < K; i++) {
+                lotto[i] = Integer.parseInt(st.nextToken());
+            }
+            generateCombinations(new ArrayList<>(), 0);
+            System.out.println(); // 각 테스트 케이스 사이에 빈 줄 출력
+        }
+    }
+
+    private static void generateCombinations(List<Integer> combination, int start) {
+        if(combination.size() == 6) {
+            for(int num : combination) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i < K; i++) {
+            combination.add(lotto[i]);
+            generateCombinations(combination, i + 1);
+            combination.remove(combination.size() - 1); // 마지막 요소 제거
+        }
+    }
+}
