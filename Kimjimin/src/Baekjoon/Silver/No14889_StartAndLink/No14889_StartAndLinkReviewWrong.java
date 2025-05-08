fd6eb96300e5907cb1b6a55329850b70f117d45d
SuJeKim <127725816+SuJeKim@users.noreply.github.com>
2025-05-08 23:54:41 +0900

    0508(목)_스타트와 링크 (#183)
    * 로ㅗì컬 ë²변
    경 사항 커밋
    
    * 김지민_pull (오류 수정 후 pull)
    
    * 로컬에 .gitattributes 추가
    
    * 0508(목)_스타트와 링크

---
+++ Kimjimin/src/Baekjoon/Silver/No14889_StartAndLink/No14889_StartAndLinkReviewWrong.java
@@ -0,0 +1,69 @@
+package Baekjoon.Silver.No14889_StartAndLink;
+
+import java.io.*;
+import java.util.*;
+
+public class No14889_StartAndLinkReviewWrong {
+
+	static int n;
+	static int[][] arr;
+	static boolean[] visited;
+	static int result = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws  IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		
+		arr = new int[n][n];
+		visited = new boolean[n];
+		
+		StringTokenizer st = null;
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		dfs(0,0);
+		System.out.println(result);
+
+	}
+
+	private static void dfs(int depth, int start) {
+		if(depth == n / 2) {
+			diff();
+			return;
+		}
+		visited[depth] = true;
+		dfs(depth + 1, start + 1);
+		visited[depth] = false;
+		dfs(depth + 1, start + 1);
+		
+	}
+
+	private static void diff() {
+		int start = 0;
+		int link = 0;
+		
+		for(int i = 0; i < n -1; i++) {
+			for(int j = i + 1; j < n; j++) {
+				if(visited[i] == true && visited[j] == true
+						) {
+					start += arr[i][j];
+					start += arr[j][i];
+				}else if(visited[i] == false && 
+						visited[j] == false) {
+					link += arr[i][j];
+					link += arr[j][i];
+				}
+			}
+		}
+		int val = Math.abs(start - link);
+		if(val == 0) {
+			System.out.println(0);
+			System.exit(0);
+		}
+		result = Math.min(result, val);
+	}
+
+}
