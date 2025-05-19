a8f1bfd5129366bf721a36ae65d8696027a3a593
jimin Kim <gjimin26@gmail.com>
2025-05-19 16:42:49 +0900

    0519(월)_섬의 개수

---
+++ Kimjimin/src/Baekjoon/Silver/No4963_NumberOfIslands/No4963_NumberOfIslands.java
@@ -0,0 +1,64 @@
+package Baekjoon.Silver.No4963_NumberOfIslands;
+
+import java.io.*;
+import java.util.*;
+
+public class No4963_NumberOfIslands {
+
+	static int w,h;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1 };
+	static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			
+			if(w == 0 && h == 0) break;
+			
+			map = new int[h][w];
+			visited = new boolean[h][w];
+			
+			for(int i = 0; i < h; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for(int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int count = 0;
+			for(int i = 0; i < h; i++) {
+				for(int j = 0; j < w; j++) {
+					if(!visited[i][j] && map[i][j] == 1) {
+						dfs(i,j);
+						count++;
+					}
+				}
+			}
+			System.out.println(count);
+		}
+
+	}
+
+	// 상하좌우,대각선 탐색.
+	private static void dfs(int x, int y) {
+		visited[x][y] = true;
+		
+		for(int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx >= 0 && ny >= 0 && nx < h && ny < w) {
+				if(!visited[nx][ny] && map[nx][ny] == 1) {
+					dfs(nx, ny);
+				}
+			}
+		}
+	}
+
+}
